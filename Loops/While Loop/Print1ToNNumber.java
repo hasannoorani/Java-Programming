6073b96a05963b7139d1f84011ada08ecca36361
Hasan Noorani <151462401+hasannoorani@users.noreply.github.com>
2024-06-11 20:54:11 +0530

    Add files via upload

---
+++ Loops/While Loop/Print1ToNNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Print1ToNNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int range = sc.nextInt();
+            int counter = 1;
+            while(counter <= range){
+                System.out.print(counter +" ");
+                counter++;
+            }
+    }  
+}
---
+++ Loops/While Loop/PrintHW100Times.java
@@ -0,0 +1,9 @@
+public class PrintHW100Times{
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+    }
+}
